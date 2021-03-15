0b3c54be230e1cf91ae2e5c9ed90f06cfbea8dea
ronLabs <ron.altamir@outlook.com>
2021-03-15 03:48:43 +0000

    Adding conversion file

---
+++ conversion.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+int print_char(va_list arg)
+{
+	char letter;
+
+	letter = va_arg(arg, int);
+	_putchar(letter);
+	return (1);
+}
+
+int print_string(va_list arg)
+{
+	char *string;
+	int i = 0;
+
+	string = va_arg(arg, char*);
+
+	while (string[i])
+	{
+	_putchar(string[i]);
+	i++
+	}
+	return (i);
+}
+
+int print_percent(__attribute__((unused))va_list arg)
+{
+	_putchar('%');
+	return(1);
+}
